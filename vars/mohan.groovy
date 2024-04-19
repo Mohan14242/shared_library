4a370b97f66de40da82f167a6283ec87ec58fbfc
mohan <mohan14242@gmail.com>
2024-04-19 19:27:33 +0530

    mohan

--- vars/mohan.groovy
+++ vars/mohan.groovy
@@ -1,9 +1,12 @@
-pipeline {
-    agent any
-    stages {
-        stage('build') {
-            steps {
-                sh 'echo "this is the mohan"'
+#!groovy 
+pipeline{
+    agent any 
+    stages{
+        stage("build "){
+            steps{
+                script{
+                    echo " this is the mohane"
+                }
             }
         }
     }
