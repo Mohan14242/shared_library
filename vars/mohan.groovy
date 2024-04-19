38536003e2fcf9af62746a6bc5acfa985a763bb9
mohan <mohan14242@gmail.com>
2024-04-19 21:58:23 +0530

    chiru

--- vars/mohan.groovy
+++ vars/mohan.groovy
@@ -1,13 +1,23 @@
 // mohan.groovy
-
-def names(Map maps) {
+pipeline{
+    agent any
+    def names(Map maps) {
     def prod_deploy = maps.prod_deploy
     def environment = maps.environment
     def choice = maps.choice
     def password = maps.password
     
-    echo "Enable debug mode: ${prod_deploy}"
-    echo "Environment: ${environment}"
-    echo "Browser: ${choice}"
-    echo "Database Password: ${password}"
+    stages{
+        stage('build'){
+            steps{
+                script{
+                echo "Enable debug mode: ${prod_deploy}"
+                echo "Environment: ${environment}"
+                echo "Browser: ${choice}"
+                echo "Database Password: ${password}"
+                }
+            }
+        }
+    }
+}
 }
