6cc085d773bba0f3f8a3623e0db33eedaa7938a1
mohan <mohan14242@gmail.com>
2024-04-19 17:43:33 +0530

    mohan

--- vars/mohan.groovy
+++ vars/mohan.groovy
@@ -4,7 +4,7 @@ pipeline{
         stage('build'){
             steps{
                 script{
-                echo "this is the mohan"
+                sh ' echo "this is the mohan"'
                 }
             }
         }
