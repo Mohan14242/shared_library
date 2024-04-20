6cafd91fcf095ad677486319a754c19be8c64452
mohan <mohan14242@gmail.com>
2024-04-20 21:10:50 +0530

    mohan

--- vars/chiru.groovy
+++ vars/chiru.groovy
@@ -1,12 +1,5 @@
-def names(Map chiru){
-    def name=chiru["name"]
-    def age=chiru.age 
-    def salary=chiru.salary
-    echo "rhis is the values under the mohan${name} and ${age} and ${salary}"
-    
-
-
-
-     
-
+def list_values(List values){
+    for (i in values){
+        println("the values are ${i}")
+    }
 }
