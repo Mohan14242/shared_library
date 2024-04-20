7545ec4169e6e39eabd5ba54db114f42f7ec34b5
mohan <mohan14242@gmail.com>
2024-04-20 20:44:22 +0530

    mohan

--- vars/mohan.groovy
+++ vars/mohan.groovy
@@ -1,5 +1,5 @@
 def exam(){
-    def names="mohan is the king "
-    def values="mohan is the waster fellow"
-    echo "this is themoha ${names} and ${values}"
+      def str = "hello, world!"
+      def upper = str.toUpperCase()
+      println("Uppercase: $upper")
 }
