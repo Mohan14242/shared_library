c5041ee624484cda43d23db71961194f667fa4a5
mohan <mohan14242@gmail.com>
2024-04-20 20:49:07 +0530

    mohan

--- vars/mohan.groovy
+++ vars/mohan.groovy
@@ -1,5 +1,10 @@
 def exam(){
       def str = "hello, world!"
       def upper = str.toUpperCase()
-      println("Uppercase: $upper")
+      def length=str.length()
+      def sub=str.substring(1,6)
+      def lower=str.lower()
+      def repl=str.replace("hello","hi")
+      def split=str.split(",")
+      println("the upper case ${upper} and length is ${length} and sub string is ${sub}")
 }
