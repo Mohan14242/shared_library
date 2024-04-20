209bf561224ea0e3dcf45e355a431aeddfd039c4
mohan <mohan14242@gmail.com>
2024-04-20 20:29:35 +0530

    mohan

--- vars/chiru.groovy
+++ vars/chiru.groovy
@@ -1,16 +1,17 @@
-def names(){
-    def values="this is the mohan  who are you "
-    def keys="this are the keys from the mohan"
-    pipeline{
-        stages{
-            stage("build"){
-                steps{
-                    script{
-                        echo "this are the keys and values ${values} and ${keys}"
+def names() {
+    def values = "this is the mohan who are you"
+    def keys = "these are the keys from the mohan"
+
+    pipeline {
+        agent any
+        stages {
+            stage("build") {
+                steps {
+                    script {
+                        echo "These are the keys and values: ${values} and ${keys}"
                     }
                 }
             }
         }
     }
-
 }
