56cbd4549871d5e896668af2851d646ce39c90f0
mohan <mohan14242@gmail.com>
2024-05-03 21:04:54 +0530

    mohan

--- vars/chiru.groovy
+++ vars/chiru.groovy
@@ -1,9 +1,13 @@
-// my-shared-library/vars/mySharedFunction.groovy
-def call(param1, param2) {
-    // Your logic here
-    println "Parameter 1: ${param1}"
-    println "Parameter 2: ${param2}"
-    // Example logic: Concatenate parameters
-    def result = "${param1}-${param2}"
-    return result
+def call(){
+    pipeline{
+        stages{
+            stage('bulding '){
+                steps{
+                    script{
+                        echo "this is the pipeline"
+                    }
+                }
+            }
+        }
+    }
 }
