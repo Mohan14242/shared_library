40fe1bb61a02db0fbca916726ad3e71a4ce5ea84
mohan <mohan14242@gmail.com>
2024-05-14 13:08:40 +0530

    mohan

--- vars/chiru.groovy
+++
@@ -1,6 +0,0 @@
-def details(Map person){
-    def name=person["name"]
-    def age=person["age"]
-    echo "the name is ${name}"
-    echo "the details are ${age}"
-}
--- vars/mohan.groovy
+++
@@ -1,4 +0,0 @@
-def fuck(String name){
-    def n1=name
-    echo "this stage is fuck stage and gets fuck her${n1}"
-}
